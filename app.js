340c999177208c037b612b8b9df4ce4354c0d9bc
Nargiza <nargiza.abnazarova@gmail.com>
2022-04-18 06:25:53 +0600

    modified repo.html

---
+++ app.js
@@ -0,0 +1,116 @@
+// 1 premennyi
+//const firstName = 'Nargiza'
+//const lastName = 'Abnazarova'
+// let age = '26'
+//const isProgrammer = 'true'
+
+
+//const $ = 'some value'
+// const if = 'ghk' // error
+//const withNum5 = '5'
+// const 5withNum =5 // error
+
+// 2 Mutirovanie
+//console.log('Имя человека: ' + firstName + ',a возраст человека: ' + age.toString())
+//const lastName = prompt (message: 'Введите ваш имя')
+//alert(firstName + ' ' + lastName)
+
+// 3 Operatri
+//let currentYear = 2020
+//const birthYear = 1995
+//const age = currentYear - birthYear
+//const a = 10
+//const b = 5
+
+//let c = 32
+//c = c + a
+
+
+// 4 Type dannih
+//const isProgrammer = true
+//const name ='Nargiza'
+//const age = 26
+//let x
+//console.log(typeof isProgrammer)
+//console.log(typeof name)
+//console.log(typeof age)
+//console.log(typeof x)
+//console.log(null)
+
+
+// 5 Prioritet operatorov
+//const fullAge = 26
+//const birtYear = 1995
+//const currentYear = 2020
+//const isFullAge = currentYear - birtYear >= fullAge // 26 >= 27 => true
+//console.log(isFullAge)
+
+// 6 Uslovnie operatori
+//const courseStatus = 'fail' //ready, fail, pending
+//if(courseStatus === 'ready') {
+    //console.log('Курс уже готов и его можно уже проходить')
+//} else if (courseStatus ==='pending'){
+    //console.log ('Курс пока находится в процессе разработки')
+//} else {
+    //console.log('Курс загружается')
+//}
+//const isReady = false
+
+//if (isReady){
+   //console.log('Vse gotovo')
+//} else {
+    //console.log('Vse ne gotovo')
+//}
+
+
+//const num1 = 42 //number
+//const num2 = '42' //string
+
+//console.log(num1 === num2)
+
+
+// 7 Bulevaya logika
+
+// 8 Function
+ //function calculateAge(year){
+ //   return 2021 - year
+// }
+
+//const myAge = calculateAge( 1995)
+//console.log(myAge)
+
+//function logInfoAbout(name, year) {
+  //  const age = calculateAge(year)
+  //  if (age > 0){
+ //       console.log('Человек по имени ' + name + ' сейчас имеет возраст' + age)
+ //   } else{
+ //       console.log('It is future information')
+//    }
+
+
+//}
+//logInfoAbout('Nargiza', 1995)
+
+// 9 Massivi
+//const cars = ['Mazda', 'BWM', 'Toyota']
+//console.log(cars)
+
+//const cars = new Array('Mazda, BWM, Toyota')
+//console.log(cars.length)
+//console.log(cars[1])
+//console.log(cars[0])
+//console.log(cars[2])
+//cars[0] = 'KIA'
+//console.log(cars)
+//cars[3] = 'Niva'
+//console.log(cars)
+
+// 10 SIKL
+//const cars = ['Mazda', 'BWM', 'Toyota', 'KIA']
+//for (let i = 0; i <cars.length; i++){
+ //  const car = cars [i]
+    //console.log(car)
+//}
+//for(let car of cars){
+ //   console.log(car)
+//}
